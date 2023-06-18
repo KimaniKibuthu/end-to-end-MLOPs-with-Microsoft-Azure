495532f2003ec29eb5d04c5bbb5e86337b0a986c
KimaniKibuthu <samp.kimani@gmail.com>
2023-06-18 15:02:48 +0200

    Challenge 0 completed

--- src/model/train.py
+++ src/model/train.py
@@ -3,16 +3,16 @@
 import argparse
 import glob
 import os
-
+import mlflow
 import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
-
+from sklearn.model_selection import train_test_split
 
 # define functions
 def main(args):
     # TO DO: enable autologging
-
+    mlflow.autolog()
 
     # read data
     df = get_csvs_df(args.training_data)
@@ -22,6 +22,9 @@ def main(args):
 
     # train model
     train_model(args.reg_rate, X_train, X_test, y_train, y_test)
+    
+    # see if the training procedure was successful
+    print('Model Trained Successfully')
 
 
 def get_csvs_df(path):
@@ -34,7 +37,10 @@ def get_csvs_df(path):
 
 
 # TO DO: add function to split data
-
+def split_data(df):
+    X, y = df[['Pregnancies','PlasmaGlucose','DiastolicBloodPressure','TricepsThickness','SerumInsulin','BMI','DiabetesPedigree','Age']].values, df['Diabetic'].values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    return X_train, X_test, y_train, y_test
 
 def train_model(reg_rate, X_train, X_test, y_train, y_test):
     # train model
@@ -57,7 +63,7 @@ def parse_args():
     # return args
     return args
 
-# run script
+# run scriptregreg
 if __name__ == "__main__":
     # add space in logs
     print("\n\n")
